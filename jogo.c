3b7674b468ce7cffdc923d750051b0cdcf424a75
Diego Arroxelas <151897145+Diegoarroxelas@users.noreply.github.com>
2024-08-31 18:46:19 -0300

    Add files via upload

--- jogo.c
+++ jogo.c
@@ -6,29 +6,40 @@ int main() {
     printf("******************************************\n");
 
     int numerosecereto = 42;
-    int chute;
 
-for(int i = 1; i <= 3; i++) {       
-    printf("Tentativa %d de 3\n",i);
+    int chute;
+    int ganhou = 0; 
+    int tentativas = 1;
+
+while(ganhou == 0) {       
+    printf("Tentativa %d \n",tentativas);
     printf("Informe seu chute: ");
-     scanf("%d",&chute);
+
+    scanf("%d",&chute);
     printf("Seu chute foi %d \n",chute);
     int acertou = (chute == numerosecereto);
-     if(acertou){
-       printf("Parabens, Voce acertou!\n"); 
-       printf("Jogue de novo, voce e um bom jogador!");
-        }else{
-          int maior = (chute > numerosecereto);
-          if(maior){
-               printf("Seu chute foi maior que o numero secreto\n");    
+    if(chute < 0){
+        printf("Voce nao pode chutar Numeros Negativos!\n");
+        continue;
+    }
+    if(acertou){
+        printf("Parabens! Voce acertou!\n"); 
+        printf("Jogue de novo, voce e um bom jogador!\n");
+
+        ganhou = 1;
+    }else{
+        int maior = (chute > numerosecereto);
+        if(maior){
+            printf("Seu chute foi maior que o numero secreto\n");    
             }
             int menor = (chute < numerosecereto);
             if(menor){
-                printf("Seu chute foi menor que o numero secreto");
+                printf("Seu chute foi menor que o numero secreto\n");
             }
-           
+        tentativas++;
     } 
   }
-   printf("Fim de jogo!")
+   printf("Fim de jogo!\n");
+   printf("Voce acertou em %d tentativas!",tentativas - 1);
 
 }
